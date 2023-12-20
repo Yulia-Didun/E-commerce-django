0b158cd90e859160c604d3279e753924e74f4781
Yulia Didun <102674126+Yulia-Didun@users.noreply.github.com>
2023-12-20 15:01:59 +0200

    Added 'category' app and 'Category' model

---
+++ category/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length = 50, unique = True)
+    slug = models.CharField(max_length = 50, unique = True)
+    description = models.TextField(max_length = 250, blank = True)
+    category_image = models.ImageField(upload_to = 'images/categories', blank = True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.category_name
+    
