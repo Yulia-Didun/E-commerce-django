ab34353b61044288bce06a6551de664fac890069
Yulia Didun <102674126+Yulia-Didun@users.noreply.github.com>
2023-12-28 05:25:44 +0200

    Added pagination and search functionalities.

--- store/views.py
+++ store/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
@@ -14,13 +16,19 @@ def store(request, category_slug=None):
     if category_slug is not None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 6) # Show 6 products per page.
+        page_number = request.GET.get('page')
+        page_products = paginator.get_page(page_number)
         products_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        paginator = Paginator(products, 6) # Show 6 products per page.
+        page_number = request.GET.get('page')
+        page_products = paginator.get_page(page_number)
         products_count = products.count()
 
     context = {
-        'products': products,
+        'products': page_products,
         'products_count': products_count,
     }
 
@@ -41,3 +49,15 @@ def product_detail(request, category_slug, product_slug):
     }
 
     return render(request, 'store/product_detail.html', context)
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_at').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            products_count = products.count()
+    context = {
+        'products': products,
+        'products_count': products_count,
+    }
+    return render(request, 'store/store.html', context)
